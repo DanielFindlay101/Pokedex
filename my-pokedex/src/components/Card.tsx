6c946c1e46d3f030b4d8a6c21710409ff0879ee2
Daniel Findlay <danielfindlay@Daniels-MBP.homenet.telecomitalia.it>
2023-06-21 11:37:00 +0200

    added descriptions

--- my-pokedex/src/components/Card.tsx
+++ my-pokedex/src/components/Card.tsx
@@ -8,18 +8,28 @@ interface CardProps {
 
 export default function Card({ pokemonIndex }: CardProps) {
   const [description, setDescription] = useState('')
+  const [type, setType] = useState('')
   
   const getPokemonDescription = async(id: string) => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+   await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
       .then((res) => {
         setDescription(res.data.flavor_text_entries[1].flavor_text)
       })
   }
 
+  const getPokemonType = async (id: string) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then((res) => {
+      setType(res.data.types[0].type.name)
+    })
+  }
+
   useEffect(() => {
     getPokemonDescription(pokemonIndex)
+    getPokemonType(pokemonIndex)
   }, [pokemonIndex])
 
+ 
 
   return (
   <div className="card w-96 bg-slate-200 shadow-xl rounded-2xl"> 
@@ -31,7 +41,8 @@ export default function Card({ pokemonIndex }: CardProps) {
        <div className="card-body">
         <h2 className="card-title"></h2>
         <p>{description}</p>
-        <div className="card-actions justify-end">
+        <div className="card-actions bg-red-200 justify-between items-center rounded-md">
+          <span className="pl-2">Type: {type}</span>
          <button className="btn btn-secondary rounded-md">
           <span className="text-red-500 text-3xl hover:text-red-300">❤️</span>  
          </button>
--- my-pokedex/src/components/Header.tsx
+++ my-pokedex/src/components/Header.tsx
@@ -1,8 +1,11 @@
 
 export default function Header() {
   return (
-    <div className='flex w-full justify-center p-6'>
+    <div className='flex w-full justify-around items-center p-2'>
         <h1 className='text-black text-lg'>POKEDEX</h1>
+        <p className="bg-secondary px-4 rounded-md text-3xl cursor-pointer text-red-500">
+          ❤️
+        </p>
     </div>
   )
 }
