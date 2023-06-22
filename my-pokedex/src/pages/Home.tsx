06c21027dbf60f8c29f57d98e822fdec77b95769
Daniel Findlay <danielfindlay@Daniels-MBP.homenet.telecomitalia.it>
2023-06-22 13:14:34 +0200

    created notification component

--- my-pokedex/src/pages/Home.tsx
+++ my-pokedex/src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header'
+import Notification from '../components/Notification'
 import Select from '../components/Select'
 
 export default function Home() {
   return (
     <>
+     {/* <Notification /> */}
      <Header />
      <Select />
     </>
